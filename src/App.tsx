83b2fd9070cd7f7e59c9bbcc06fbef51174f09aa
NickTahmasebi <nicktahmasebi@hotmail.com>
2023-03-13 16:15:31 +0100

    Everything updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import itemsJSON from './Data/ProductList.json'
 import './App.css'
 import items from "./Data/ProductList.json"
 import "bootstrap/dist/css/bootstrap.min.css"
-import {Col, Row} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 import {CheckoutItem} from "./Components/StoreItem";
 import {ShoppingCartProvider} from "./context/ShoppingCartContext";
 import Header from "./Components/Header";
@@ -12,25 +12,21 @@ import Items from "./Components/Items";
 import Basket from "./Components/Basket";
 import SearchBar from "./Components/SearchBar";
 import Billing from "./Components/Billing";
+import {Route, Routes} from 'react-router-dom'
+import {Home} from "./Pages/Home"
+import {Checkout} from "./Pages/Checkout"
 
 
 
 
 function App() {
     return (
-
-        <ShoppingCartProvider>
-            <div><Header></Header>
-            <SearchBar></SearchBar>
-                <div className="row">
-                    <Items></Items>
-                    <Basket></Basket>
-                </div>
-            </div>
-            <Billing></Billing>
-
-        </ShoppingCartProvider>
-
+        <Container>
+            <Routes>
+                <Route path = "/" element={<Home/>} />
+                <Route path = "/Checkout" element={<Checkout/>} />
+            </Routes>
+        </Container>
 
     )
 }
