4145258a2ca950f303f428a21b884137ba3845e4
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-04-17 18:25:27 +0200

    StoreItem.tsx with our own css

--- src/Components/StoreItem.tsx
+++ src/Components/StoreItem.tsx
@@ -2,7 +2,7 @@ import {Button, Card} from "react-bootstrap"
 import {useShoppingCart} from "../context/ShoppingCartContext";
 import {Product} from "./Items";
 
-export function CheckoutItem({id, name, price, currency, rebateQuantity, rebatePercent, imageUrl}: Product) {
+export function CheckoutItem({id, name, price, imageUrl}: Product) {
     const {incrementItem} = useShoppingCart()
     return (
         <Card className="product-card">
@@ -11,12 +11,13 @@ export function CheckoutItem({id, name, price, currency, rebateQuantity, rebateP
                 src={`https://raw.githubusercontent.com/Sofiedige/node/main/public${imageUrl}`}
                 height="100px"
                 className="product-card__image"
+                style={{objectFit: "cover"}}
             />
-            <Card.Body className="d-flex flex-column">
-                <div className="d-flex justify-content-between align-items-baseline mb-4">
+            <Card.Body className="card-body">
+                <Card.Title className="card-title">
                     <span className="product-card__name">{name}</span>
                     <span className="product-card__price">{price} kr.</span>
-                </div>
+                </Card.Title>
                 <Button
                     className="product-card__button"
                     onClick={() => {
