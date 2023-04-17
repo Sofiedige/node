959e7d7b023222f9d66f12a554262d666106d45d
MarcoHansen7 <s194302@student.dtu.dk>
2023-04-17 16:56:29 +0200

    flex pictures and text no wrap

--- src/Components/StoreItem.tsx
+++ src/Components/StoreItem.tsx
@@ -4,27 +4,26 @@ import {Product} from "./Items";
 
 export function CheckoutItem({id, name, price, imageUrl}: Product) {
     const {incrementItem} = useShoppingCart()
-    return (
-        <Card className="product-card">
-            <Card.Img
-                variant="top"
-                src={`https://raw.githubusercontent.com/Sofiedige/node/main/public${imageUrl}`}
-                className="product-card__image"
-            />
-            <Card.Body className="card-body">
-                <div className="d-flex justify-content-between align-items-baseline mb-4">
-                    <span className="product-card__name">{name}</span>
-                    <span className="product-card__price">{price} kr.</span>
-                </div>
-                <Button
-                    className="product-card__button"
-                    onClick={() => incrementItem(id)}
+
+    return <Card className="primaryColor">
+        <Card.Img
+            variant="top"
+            src = {`https://raw.githubusercontent.com/Sofiedige/node/main/public${imageUrl}`}
+            height="180px"
+            style={{objectFit: "cover"}}
+        />
+        <Card.Body className="card-body">
+            <Card.Title className="card-title">
+                <span className="fs-5">{name}</span>
+                <span className="ms-4 text-muted">{price}{" kr."}</span>
+
+            </Card.Title>
+            <Button className="add-to-cart-button" onClick={() => incrementItem(id)}
                     variant="success"
                 >
                     Add to cart
                 </Button>
             </Card.Body>
         </Card>
-    );
 }
 
