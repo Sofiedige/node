8161bc23af26050eb68c765987d000b4cffb772e
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-04-24 17:09:26 +0200

    no bootstrap. Instead using css

--- src/Components/StoreItem.tsx
+++ src/Components/StoreItem.tsx
@@ -6,11 +6,10 @@ export function CheckoutItem({id, name, price, imageUrl}: Product) {
     const {incrementItem} = useShoppingCart()
     return (
         <Card className="product-card">
-            <Card.Img
+            <Card.Img className="product-card_image"
                 variant="top"
                 src={`https://raw.githubusercontent.com/Sofiedige/node/main/public${imageUrl}`}
                 height="100px"
-                className="product-card__image"
                 style={{objectFit: "cover"}}
             />
             <Card.Body className="card-body">
