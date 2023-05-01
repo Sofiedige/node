1800fb80b87ab90d70933b0a7ed55496781549e0
Sofiedige <sofiedige1@gmail.com>
2023-05-01 14:44:48 +0200

    goback - button

--- src/Pages/Checkout.tsx
+++ src/Pages/Checkout.tsx
@@ -1,12 +1,14 @@
 import Billing from "../Components/Billing";
 import {ShoppingCartProvider} from "../context/ShoppingCartContext";
 import Header from "../Components/Header";
+import {navigate} from "../App";
 
 export function Checkout(){
     return <ShoppingCartProvider>
         <div>
             <Header></Header>
             <div className="row">
+
              <Billing></Billing>
             </div>
         </div>
