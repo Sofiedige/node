1389021fb3a896ade70d1d8316a75ac1c5e431be
Sofiedige <sofiedige1@gmail.com>
2023-05-01 12:03:18 +0200

    Verification page done

--- src/Components/ConfirmedVerification.tsx
+++ src/Components/ConfirmedVerification.tsx
@@ -9,10 +9,10 @@ export default function ConfirmedVerification() {
             <p>
                 <BasketInCheckout/>
             </p>
-            <div className="center">
-                <img className="billede" src="verification.png" alt="verificationlogo"/>
+            <div className="row-middle">
+                <img className="verificationimage" src="verification1.png" alt="verificationlogo"/>
                 <h2>Thank you!</h2>
-                <h1>We're very glad that you choose to use Juice Jamboree, we will pack your items as soon as
+                <h1>We're very glad that you choose to use Juicy Jamboree, we will pack your items as soon as
                     possible.</h1>
                 <div>
                     <button className="continue-button" onClick={(event) => navigate(event, "home")}>
