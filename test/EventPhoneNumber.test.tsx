e4a19fc7fa211ea636a0b565c29e1c5657f573a3
Hussein El-Zein <husseinelzeinprivat@gmail.com>
2023-05-01 16:02:20 +0200

    fixed tests, fixed header to have link

--- test/EventPhoneNumber.test.tsx
+++ test/EventPhoneNumber.test.tsx
@@ -1,36 +1,29 @@
-import React, {useState} from "@types/react";
+import { describe, expect, it } from "vitest";
+import { useState } from "react";
+import { isValidPhoneNumber } from "../src/Components/Billing";
 
 describe("handlePhoneNumberChange", () => {
-
     const [phoneNumber, setPhoneNumber] = useState<string>("");
 
-    function isValidPhoneNumber(phoneNumber: string): boolean {
-        const regex = /^(\+|\d)[0-9]*$/;
-        return regex.test(phoneNumber);
-    }
-
-    const handlePhoneNumberChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const handlePhoneNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newPhoneNumber = event.target.value;
 
         if (newPhoneNumber.length === 0) {
-            setPhoneNumber("")
-
+            setPhoneNumber("");
         } else if (isValidPhoneNumber(newPhoneNumber)) {
             setPhoneNumber(newPhoneNumber);
         }
-    }
+    };
 
-    test("should update the phone number state when a user types a new number", async () => {
-
-        const event = {
+    it("should update the phone number state when a user types a new number", () => {
+        const event: React.ChangeEvent<HTMLInputElement> = {
             target: {
-                value: "123-456-7890",
+                value: "+123-abcd456-78ahgf90",
             },
-        };
+        } as React.ChangeEvent<HTMLInputElement>;
 
-        await handlePhoneNumberChange(event, setPhoneNumber);
+        handlePhoneNumberChange(event);
 
-        print("ph")
-        expect(phoneNumber).toBe("1234567890");
+        expect(phoneNumber).toBe("+1234567890");
     });
 });
